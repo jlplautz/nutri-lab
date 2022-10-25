663926a6b1e573a942a9f8adabc1034706e71a2b
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2022-10-25 00:19:32 -0300

    2-create_plaform_Application

--- pyprg/urls.py
+++ pyprg/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('pyprg.authentication.urls')),
+    path('', include('pyprg.plataforma.urls')),
 ]
